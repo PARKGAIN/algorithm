458eb19c089a95ec0adce57b927f858f1d7fbf30
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-06-18 23:00:58 +0900

    동전문제 푸는 중

---
+++ Boj11047.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Coin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N =10;
+		long K =Integer.parseInt(sc.nextLine());
+		// 프로그램으로 짜는 것이 어렵다...
+		//언제까지? 나머지가 0일때까지 
+		
+	}
+}

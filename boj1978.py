9d989193fa91197ad81ad1695c28dce820e437cf
strider <84880886+PARKGAIN@users.noreply.github.com>
2023-05-02 17:39:36 +0900

    docs: 소수찾기문제(지피티한테 배움...)

--- boj1978.py
+++ boj1978.py
@@ -1,3 +1,13 @@
-N=int(input());
-arr=list(map(int,input().split()))
-ans=[]
+N = int(input())
+arr = list(map(int, input().split()))
+
+def isSosu(x):
+  if x < 2:
+    return False
+  for i in range(2, int(x**0.5)+1):
+    if x % i == 0:
+      return False
+  return True
+
+ans = list(filter(isSosu, arr))
+print(len(ans))
